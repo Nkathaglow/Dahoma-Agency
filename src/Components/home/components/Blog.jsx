c9113e36bb888ef12ca4ae6779a594016bbdc41e
eve-atieno <atienoevelyne53@gmail.com>
2023-12-07 22:05:34 +0300

    made changes

--- src/Components/home/components/Blog.jsx
+++ src/Components/home/components/Blog.jsx
@@ -6,18 +6,18 @@ import love from "../../../assets/love.svg"
 function Blog() {
   const cardsData = [
     {
-      title: 'Business strategy',
-      detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et pellentesque.',
+      title: 'Limited Resources',
+      detail: 'Startups often operate with restricted budgets, manpower, and infrastructure, making it challenging to execute plans or scale operations effectively',
       imageSrc: pyramid,
     },
     {
-      title: 'Business strategy',
-      detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et pellentesque.',
+      title: 'Product-Market Fit',
+      detail: ' Identifying the right target audience and ensuring that their product or service fulfills a  genuine need in the market is crucial but challenging.',
       imageSrc: wand,
     },
     {
-      title: 'Business strategy',
-      detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et pellentesque.',
+      title: 'Strong Brand Identit',
+      detail: '  Establishing a unique brand identity and gaining trust and credibility within the market is an ongoing challenge for startups.',
       imageSrc: love,
     },
   ];
@@ -27,7 +27,9 @@ function Blog() {
       <div className='container mx-auto'>
         <div className='grid gap-2 py-10  grid-cols-2 '>
           <p className='text-left font-semibold text-[#0d1317] text-4xl'>We are building a software solution that solves your business challenges</p>
-          <p className='text-left text-[#1d3444] text-lg font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, mattis purus. Vivamus commodo suscipit tellus et pellentesque.</p>
+          <p className='text-left text-[#1d3444] text-lg font-medium'>  At our core, we excel in crafting software solutions that not only align with your distinct
+  requirements but also surpass expectations. Our technology solutions range from blockchain
+  applications, healthcare management systems, and a spectrum of cutting-edge tools.</p>
         </div>
         <div className="grid grid-cols-3 gap-6">
           {cardsData.map((card, index) => (
